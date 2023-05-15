ab7c6899361c226af19f4172b330bb512dfeef7c
shebarolas <30813367+shebarolas@users.noreply.github.com>
2023-05-15 12:01:38 -0400

    Actualizacion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Hero from '../components/Home/Hero'
+import nosotros from '../imgs/nosotros.jpg'
+import '../styles/home.css'
 
 export const Home = () => {
   return (
     <>
-        <Hero/>
+      <Hero />
+      <div className="globals">
         <div className="nosotros">
-            <h1 className='h1'>Hola</h1>
+          <h1 className='h1'>¿Quiénes Somos?</h1>
+          <div className="infos">
+            <div className="detalle">
+              <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita quam error accusamus aliquam dolorum nobis rerum atque consectetur
+                fugiat necessitatibus,
+                deleniti ad fugit rem cum saepe, animi ducimus labore!</span>
+            </div>
+            <div className="imagen">
+              <img src={nosotros} alt="Nosotros 1" />
+            </div>
+          </div>
         </div>
+      </div>
+
     </>
   )
 }
