4ab1fb807bcb09fd1ee7fabe64ea9bc33c0e33ed
shebarolas <30813367+shebarolas@users.noreply.github.com>
2023-10-12 13:54:46 -0300

    final

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -6,14 +6,14 @@ import '../styles/footer.css';
 export const Foot = () => {
   return (
     <div className='footer'>
-         <div className="informacions">
-                <img src={logo} alt="" className='fotLogo' />
-                <span><a href="mailto:cafeteriacabanaseluney@gmail.com">@cafeteriacabanaseluney@gmail.com</a></span>
-                <span>@ Whatsapp: +56995416124</span>
-        </div>
+      <div className="informacions">
+        <img src={logo} alt="" className='fotLogo' />
+        <span><a href="mailto:cafeteriacabanaseluney@gmail.com">@cafeteriacabanaseluney@gmail.com</a></span>
+        <span>@ Whatsapp: +56995416124</span>
         <div className="logos">
-                <a href="https://www.facebook.com/profile.php?id=100093055598485"><FaFacebook size={30} /></a>
+          <a href="https://www.facebook.com/profile.php?id=100093055598485"><FaFacebook size={30} /></a>
         </div>
+      </div>
     </div>
   )
 }
