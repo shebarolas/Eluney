6dfbfb3a27201270256280b0ba25788f345e5c36
shebarolas <30813367+shebarolas@users.noreply.github.com>
2023-11-24 10:16:20 -0300

    final

--- src/components/Gallery/ModalsR.jsx
+++ src/components/Gallery/ModalsR.jsx
@@ -217,7 +217,7 @@ export const ModalsR = () => {
                                     Se encuentra a 26 km de Geometric Hot Springs y ofrece terraza y aparcamiento privado gratuito.
                                 </span>
                                 <span className='cabInfo blod'>1 dormitorios</span>
-                                <span className='cabInfo blod'>Valor Referencial: 60.000 clp</span>
+                                <span className='cabInfo blod'>Valor Referencial: 60.000 clp por noche</span>
                                 <span className="cabInfo blod">
                                     El Checkin para las cabañas debe ser despues de las 15:00 hrs.
                                 </span>
